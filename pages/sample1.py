11dc881f67baad74e3e63173dee918daf65d1c21
scalalang2 <cailhuiris@gmail.com>
2025-01-01 08:32:14 +0900

    Add Table Editor viewer

--- pages/sample1.py
+++ pages/sample1.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Hello, world!")
+df = pd.read_excel('sample.xlsx')
+
+st.title('Excel Table')
+edited_df = st.data_editor(df, num_rows="dynamic")
+
+if st.button('Save Changes'):
+    # edited_df.to_excel('your_excel_file_updated.xlsx', index=False)
+    st.success('Changes saved successfully!')
